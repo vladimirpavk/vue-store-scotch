e7fc6829479a3764c5603882667f1b309aad2e40
Vladimir Pavković <vladimirpavk@gmail.com>
2017-10-24 11:14:38 +0200

    Form validation

--- src/pages/admin/New.vue
+++ src/pages/admin/New.vue
@@ -3,5 +3,28 @@
     <div class="title">
       <h1>This is Admin/New</h1>
     </div>
-  </div>
+    <label for="name">Unesite ime(obavezno polje) : </label>
+    <input type="text" placeholder="Imex" v-model="model.ime" v-validate="'required'" name="name"/>
+    <div>
+      <button v-on:click="validate()">Validate</button>
+    </div>
+  </div>     
 </template>
+<script>
+  export default{
+    name: 'New',
+    data () {
+      return {
+        model: {
+          ime: 'Vlada'
+        }
+      }
+    },
+    methods: {
+      validate () {
+        console.log(this.model.ime)
+        console.log(this.errors)
+      }
+    }
+  }
+</script>
